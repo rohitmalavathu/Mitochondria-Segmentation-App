42c863f4bf1767e4d2bf8ec99ef4827e1ca27cf6
rohitmalavathu <rohitmalavathu@gmail.com>
2025-09-27 21:52:12 -0400

    Add multi-tasking support during segmentation - users can use other tools while processing

--- app.py
+++ app.py
@@ -593,5 +593,5 @@ def calculate_scale():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 9669))
     app.run(debug=False, host='0.0.0.0', port=port)
